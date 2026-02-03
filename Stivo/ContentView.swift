55587394dccf71866d9ea830879005c2733558e8
yasminalhabib <y.alhabib6789@gmail.com>
2026-02-03 09:55:41 +0300

    Initial Commit

---
+++ Stivo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stivo
+//
+//  Created by Yasmin Alhabib on 03/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StivoTests/StivoTests.swift
@@ -0,0 +1,16 @@
+//
+//  StivoTests.swift
+//  StivoTests
+//
+//  Created by Yasmin Alhabib on 03/02/2026.
+//
+
+import Testing
+
+struct StivoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
